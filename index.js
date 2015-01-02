2c29887dafe404d6cea13dd70784d82364215879
Haojian Wu <hokein.wu@gmail.com>
2015-01-02 15:18:32 +0800

    Implement Instance Methods.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 Class = function(func) {
+  var class_constructor = function() {};
   if ('initialize' in func)
-    return func['initialize'];
-  return function() {}
+    class_constructor = func['initialize'];
+  for (var name in func) {
+    if (name != 'initialize')
+     class_constructor.prototype[name] = func[name];
+  }
+  return class_constructor;
 }
 
 module.exports = Class
