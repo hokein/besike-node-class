4a06be8353a272fe165659acd4de877a7550e030
Haojian Wu <hokein.wu@gmail.com>
2015-01-03 17:22:59 +0800

    Fix infinite recursion.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//var current_class;
+
 Class = function(func, par) {
   var class_constructor = function() {};
   class_constructor.__super__ = Object;
@@ -13,10 +15,23 @@ Class = function(func, par) {
     if (name != 'initialize')
      class_constructor.prototype[name] = func[name];
   }
+
   class_constructor.prototype.super = function() {
-    return class_constructor.prototype.prototype[arguments[0]].apply(
-        this, [].slice.call(arguments,1));
-  }
+    var current_class;
+
+    return function() {
+      // Set value when first time invoked.
+      if (!current_class)
+        current_class = class_constructor;
+      var bak = current_class;
+      current_class = current_class.prototype;
+      value = current_class.prototype[arguments[0]].apply(
+          this, [].slice.call(arguments,1));
+      current_class = bak;
+      return value;
+    };
+  }();
+
   return class_constructor;
 }
 
