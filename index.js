542ec0118e3ad44a84be8142545821cec2a8e066
Haojian Wu <hokein.wu@gmail.com>
2015-01-03 16:31:27 +0800

    Implement super method.

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ Class = function(func, par) {
   if (par) {
     class_constructor.__super__ = par;
     class_constructor.prototype = new par();
+    class_constructor.prototype.prototype = par.prototype;
     class_constructor.prototype.constructor = class_constructor;
   }
   for (var name in func) {
     if (name != 'initialize')
      class_constructor.prototype[name] = func[name];
+  }
+  class_constructor.prototype.super = function() {
+    return class_constructor.prototype.prototype[arguments[0]].apply(
+        this, [].slice.call(arguments,1));
   }
   return class_constructor;
 }
