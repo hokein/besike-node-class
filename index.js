db9b69f625bad9079ac0eff691f4b296e8f3dfad
Haojian Wu <hokein.wu@gmail.com>
2015-01-03 14:47:40 +0800

    Implement methods inheritance.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-Class = function(func) {
+Class = function(func, par) {
   var class_constructor = function() {};
   if ('initialize' in func)
     class_constructor = func['initialize'];
+  if (par) {
+    class_constructor.prototype = new par();
+    class_constructor.prototype.constructor = class_constructor;
+  }
   for (var name in func) {
     if (name != 'initialize')
      class_constructor.prototype[name] = func[name];
