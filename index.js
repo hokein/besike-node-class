bac29c58981db190a2cfa98aaadb2e7fe9e0cc38
Haojian Wu <hokein.wu@gmail.com>
2015-01-04 23:53:24 +0800

    Implement John's _super method.
    Also some rename work for better understanding.

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-//var current_class;
-
-Class = function(func, par) {
-  var class_constructor = function() {};
-  class_constructor.__super__ = Object;
-  if ('initialize' in func)
-    class_constructor = func['initialize'];
-  if (par) {
-    class_constructor.__super__ = par;
-    class_constructor.prototype = new par();
-    class_constructor.prototype.prototype = par.prototype;
-    class_constructor.prototype.constructor = class_constructor;
+Class = function(func, parent) {
+  var child = func['initialize'] || function() {};
+  child.__super__ = parent || Object;
+  if (parent) {
+    child.prototype = new parent();
+    child.prototype.prototype = parent.prototype;
+    child.prototype.constructor = child;
+    child.prototype._super = child.prototype;
   }
+
   for (var name in func) {
-    if (name != 'initialize')
-     class_constructor.prototype[name] = func[name];
+    if (typeof func[name] == 'function' &&
+        typeof child.prototype[name] == 'function' &&
+        /\b_super\b/.test(func[name])) { // check exsiting parent class method.
+      child.prototype[name] = function(name, fn) {
+        var parent_method = child.prototype[name];
+        return function() {
+          var tmp = this._super;
+          this._super = parent_method;
+          var ret = fn.apply(this, arguments);
+          this._super = tmp;
+          return ret;
+        }
+      }(name, func[name]);
+    } else {
+      child.prototype[name] = func[name];
+    }
   }
 
-  class_constructor.prototype.super = function() {
+  child.prototype.super = function() {
     var current_class;
 
     return function() {
       // Set value when first time invoked.
       if (!current_class)
-        current_class = class_constructor;
+        current_class = child;
       var bak = current_class;
       current_class = current_class.prototype;
       value = current_class.prototype[arguments[0]].apply(
@@ -32,7 +43,7 @@ Class = function(func, par) {
     };
   }();
 
-  return class_constructor;
+  return child;
 }
 
 module.exports = Class
