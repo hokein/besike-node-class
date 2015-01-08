5df05e18bd16f3e6499d6a0d565d6a4f28ddfdce
Haojian Wu <hokein.wu@gmail.com>
2015-01-08 15:02:51 +0800

    Building middlewares stack and implement error handling.

--- week2/myexpress/index.js
+++ week2/myexpress/index.js
@@ -2,13 +2,63 @@ var http = require('http');
 
 module.exports = function() {
   var requestListener = function(req, res) {
-    res.statusCode = 404;
-    res.end('404 not found');
+    requestListener.current_pos = 0;
+    requestListener.req = req;
+    requestListener.res = res;
+    requestListener.next();
   }
   requestListener.listen = function() {
     var server = http.createServer(requestListener);
     server.listen.apply(server, arguments);
     return server;
   };
+  requestListener.stack = []
+  requestListener.current_pos = 0;
+  requestListener.use = function(middleware) {
+    requestListener.stack.push(middleware);
+    return requestListener;
+  }
+  requestListener.next = function(err) {
+    if (requestListener.current_pos >= requestListener.stack.length) {
+      if (err) {
+        requestListener.res.statusCode = 500;
+        requestListener.res.end('500 Internal Error');
+      }
+      else {
+        requestListener.res.statusCode = 404;
+        requestListener.res.end('404 not found');
+      }
+      return;
+    }
+    var pos = requestListener.current_pos;
+    if (err) {
+      while (pos < requestListener.stack.length) {
+        if (requestListener.stack[pos].length < 4)
+          ++pos;
+        else
+          break;
+      }
+    } else {
+      while (pos < requestListener.stack.length) {
+        if (requestListener.stack[pos].length >= 4) {
+          ++pos;
+        } else
+          break;
+      }
+    }
+    requestListener.current_pos = pos + 1;
+    try {
+      if (err) {
+        requestListener.stack[pos](err, requestListener.req, requestListener.res,
+            requestListener.next);
+      } else {
+        requestListener.stack[pos](requestListener.req, requestListener.res,
+            requestListener.next);
+      }
+    } catch (e) {
+      requestListener.res.statusCode = 500;
+      requestListener.res.end('500 Internal Error');
+    }
+  }
   return requestListener;
 }
--- week2/myexpress/test/app_spec.js
+++ week2/myexpress/test/app_spec.js
@@ -1,6 +1,7 @@
 var express = require('../');
 var request = require('supertest');
 var http = require('http');
+var expect = require("chai").expect
 
 describe('app',function() {
   describe('create http server', function() {
@@ -14,15 +15,124 @@ describe('app',function() {
       request(server).get('/').expect(404).end(done);
     });
   });
-  describe("#listen",function() {
+  describe("#listen", function() {
     var server;
     before(function() {
       var app = express();
       app.listen(7000);
     });
 
-    it('should return 404',function(done) {
+    it('should return 404', function(done) {
       request('http://localhost:7000').get('/foo').expect(404).end(done);
     });
+  });
+  describe(".use", function() {
+    var app = express();
+    var m1 = function() {};
+    var m2 = function() {};
+    app.use(m1);
+    app.use(m2);
+
+    it('should be able to add middlewares', function() {
+      expect(app.stack.length).to.eql(2);
+    });
+  });
+});
+
+describe("calling middleware stack", function() {
+  var app;
+  beforeEach(function() {
+    app = express();
+  });
+
+  it('should be able to call a single middleware', function(done) {
+    var m1 = function(req,res,next) {
+      res.end("hello from m1");
+    };
+    app.use(m1);
+    request(app).get('/').expect('hello from m1').end(done);
+  });
+
+  it('should be able to call next to go to next middleware', function(done) {
+    var m1 = function(req,res,next) {
+      next();
+    };
+    var m2 = function(req,res,next) {
+      res.end("hello from m2");
+    };
+    app.use(m1);
+    app.use(m2);
+    request(app).get('/').expect('hello from m2').end(done);
+  });
+
+  it('should return 404 at the end of middleware chain', function(done) {
+    var m1 = function(req,res,next) {
+      next();
+    };
+    var m2 = function(req,res,next) {
+      next();
+    };
+    app.use(m1);
+    app.use(m2);
+    request(app).get('/').expect(404).end(done);
+  });
+
+  it('should return 404 when middleware chain is empty', function(done) {
+    request(app).get('/').expect(404).end(done);
+  });
+});
+
+describe("Error Handling", function() {
+  var app;
+  beforeEach(function() {
+    app = new express();
+  });
+
+  it('should return 500 for unhandled error', function(done) {
+    var m1 = function(req,res,next) {
+      next(new Error("boom!"));
+    };
+    app.use(m1);
+    request(app).get('/').expect(500).end(done);
+  });
+
+  it('should return 500 for uncaught error', function(done) {
+    var m1 = function(req,res,next) {
+     throw new Error("boom!");
+    };
+    app.use(m1);
+    request(app).get('/').expect(500).end(done);
+  });
+
+  it('should skip error handlers when next is without an error', function(done) {
+    var m1 = function(req,res,next) {
+      next();
+    };
+    var e1 = function(err,req,res,next) { // timeout
+    };
+    var m2 = function(req,res,next) {
+      res.end("m2");
+    };
+    app.use(m1);
+    app.use(e1); // should skip this. will timeout if called.
+    app.use(m2);
+    request(app).get('/').expect('m2').end(done);
+  });
+
+  it('should skip normal handlers when next is with an error', function(done) {
+    var m1 = function(req,res,next) {
+      next(new Error("boom!"));
+    };
+    var m2 = function(req,res,next) {
+      // timeout
+    };
+    var e1 = function(err,req,res,next) {
+      res.end("e1");
+    };
+    app.use(m1);
+    app.use(m2); // should skip this. will timeout if called.
+    app.use(e1);
+
+    request(app).get('/').expect('e1').end(done);
   });
 });
