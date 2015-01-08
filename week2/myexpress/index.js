e8b76a10f1712d35fe2418cc5f481c0346484afd
Haojian Wu <hokein.wu@gmail.com>
2015-01-08 16:43:35 +0800

    Implement App Embedding as middleware.

--- week2/myexpress/index.js
+++ week2/myexpress/index.js
@@ -2,52 +2,76 @@ var http = require('http');
 
 module.exports = function() {
   var requestListener = function(req, res) {
-    requestListener.current_pos = 0;
+    requestListener.currentPos = 0;
     requestListener.req = req;
     requestListener.res = res;
     requestListener.next();
   }
+
+  requestListener.callAsMiddleware = function(err, req, res, parent) {
+    requestListener.currentPos = 0;
+    requestListener.req = req;
+    requestListener.res = res;
+    requestListener.parent = parent;
+    requestListener.next(err);
+  }
+
   requestListener.listen = function() {
     var server = http.createServer(requestListener);
     server.listen.apply(server, arguments);
     return server;
   };
+
   requestListener.stack = []
-  requestListener.current_pos = 0;
+  requestListener.currentPos = 0;
   requestListener.use = function(middleware) {
     requestListener.stack.push(middleware);
     return requestListener;
   }
+
+  requestListener._findNextMiddleware = function(err) {
+    function isErrorHandler(func) {
+      return func.length >= 4;
+    }
+    var nextPos = requestListener.currentPos;
+    while (nextPos < requestListener.stack.length) {
+      if (requestListener.stack[nextPos].callAsMiddleware)
+        return nextPos;
+      if (err && isErrorHandler(requestListener.stack[nextPos]))
+        return nextPos;
+      if (!err && !isErrorHandler(requestListener.stack[nextPos]))
+        return nextPos;
+      ++nextPos;
+    }
+    return nextPos;
+  }
+
   requestListener.next = function(err) {
-    if (requestListener.current_pos >= requestListener.stack.length) {
+    if (requestListener.currentPos >= requestListener.stack.length) {
+      // check whether the current app has parent in the middle chain.
+      if (requestListener.parent) {
+        requestListener.parent.next(err);
+        return;
+      }
+
       if (err) {
         requestListener.res.statusCode = 500;
         requestListener.res.end('500 Internal Error');
-      }
-      else {
+      } else {
         requestListener.res.statusCode = 404;
         requestListener.res.end('404 not found');
       }
       return;
     }
-    var pos = requestListener.current_pos;
-    if (err) {
-      while (pos < requestListener.stack.length) {
-        if (requestListener.stack[pos].length < 4)
-          ++pos;
-        else
-          break;
-      }
-    } else {
-      while (pos < requestListener.stack.length) {
-        if (requestListener.stack[pos].length >= 4) {
-          ++pos;
-        } else
-          break;
-      }
-    }
-    requestListener.current_pos = pos + 1;
+
+    var pos = requestListener._findNextMiddleware(err);
+    requestListener.currentPos = pos + 1;
     try {
+      if (requestListener.stack[pos].callAsMiddleware) {
+        requestListener.stack[pos].callAsMiddleware(err, requestListener.req,
+            requestListener.res, requestListener);
+        return;
+      }
       if (err) {
         requestListener.stack[pos](err, requestListener.req, requestListener.res,
             requestListener.next);
--- week2/myexpress/test/app_spec.js
+++ week2/myexpress/test/app_spec.js
@@ -136,3 +136,38 @@ describe("Error Handling", function() {
     request(app).get('/').expect('e1').end(done);
   });
 });
+
+describe('Implement app embedding as midddleware',function() {
+  var app;
+  var subApp;
+  beforeEach(function() {
+    app = new express();
+    subApp = new express();
+  });
+
+  it('should pass unhandled request to parent', function(done) {
+    function m1(req,res,next) {
+      res.end('m1');
+    };
+    function m2(req,res,next) {
+      res.end('m2');
+    };
+    app.use(subApp);
+    app.use(m2);
+    request(app).get('/').expect('m2').end(done);
+  });
+
+  it('should pass unhandled error to parent', function(done) {
+    function m1(req,res,next) {
+      next("m1 error");
+    };
+    function e1(err,req,res,next) {
+      res.end(err);
+    };
+
+    subApp.use(m1);
+    app.use(subApp);
+    app.use(e1);
+    request(app).get('/').expect('m1 error').end(done);
+  });
+});
