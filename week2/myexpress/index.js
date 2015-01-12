656b03580e5f5b27856be2a6f2ec0d086bb297a5
Haojian Wu <hokein.wu@gmail.com>
2015-01-12 12:26:02 +0800

    Fix race condition problem
    1. Skip all correct middleware handler and subapps when an error
    occurred.
    2. we should store all individual states for each http request to avoid race
    condition problem.
    
    In previous implementation, all the attributes will be recovered when
    serving multiple request:
    
    ```
    // Expected result:
    // m1 {n:1}
    // m1 {n:2}
    // m2 {n:1}
    // m2 {n:2}
    app.use(function(req,res,next) {
      console.log("m1",req);
      setTimeout(next,100);
    });
    
    // m2
    app.use(function(req,res,next) {
      // get `req.n == 1` twice
      console.log("m2",req);
    });
    
    // m3
    app.use(function(req,res,next) {
      console.log("shouldn't happen");
    });
    
    // send fake requests to app
    var req1 = {n: 1};
    var req2 = {n: 2};
    
    app(req1);
    app(req2);
    ```

--- week2/myexpress/index.js
+++ week2/myexpress/index.js
@@ -1,88 +1,50 @@
 var http = require('http');
 
+function isErrorHandler(func) {
+  return func.length >= 4;
+}
+
 module.exports = function() {
-  var requestListener = function(req, res) {
-    requestListener.currentPos = 0;
-    requestListener.req = req;
-    requestListener.res = res;
-    requestListener.next();
+  var requestListener = function(req, res, parentNext) {
+    var currentPos = 0;
+    function next(err) {
+      if (currentPos >= requestListener.stack.length) {
+        if (parentNext) {
+          parentNext(err);
+        } else {
+          res.statusCode = err? 500:404;
+          res.end('');
+        }
+      } else {
+        var middleware = requestListener.stack[currentPos];
+        ++currentPos;
+        try {
+          // If an error occurred, skip all subapps also.
+          if (err && isErrorHandler(middleware))
+            middleware(err, req, res, next);
+          // call next middleware or subapp
+          else if (!err && !isErrorHandler(middleware))
+            middleware(req, res, next);
+          else // skip to next middleware
+            next(err);
+        } catch(e) {
+          res.statusCode = 500;
+          res.end('');
+        }
+      }
+    }
+    next();
   }
 
-  requestListener.callAsMiddleware = function(err, req, res, parent) {
-    requestListener.currentPos = 0;
-    requestListener.req = req;
-    requestListener.res = res;
-    requestListener.parent = parent;
-    requestListener.next(err);
-  }
-
+  requestListener.stack = [];
   requestListener.listen = function() {
     var server = http.createServer(requestListener);
     server.listen.apply(server, arguments);
     return server;
-  };
-
-  requestListener.stack = []
-  requestListener.currentPos = 0;
+  }
   requestListener.use = function(middleware) {
     requestListener.stack.push(middleware);
     return requestListener;
   }
-
-  requestListener._findNextMiddleware = function(err) {
-    function isErrorHandler(func) {
-      return func.length >= 4;
-    }
-    var nextPos = requestListener.currentPos;
-    while (nextPos < requestListener.stack.length) {
-      if (requestListener.stack[nextPos].callAsMiddleware)
-        return nextPos;
-      if (err && isErrorHandler(requestListener.stack[nextPos]))
-        return nextPos;
-      if (!err && !isErrorHandler(requestListener.stack[nextPos]))
-        return nextPos;
-      ++nextPos;
-    }
-    return nextPos;
-  }
-
-  requestListener.next = function(err) {
-    if (requestListener.currentPos >= requestListener.stack.length) {
-      // check whether the current app has parent in the middle chain.
-      if (requestListener.parent) {
-        requestListener.parent.next(err);
-        return;
-      }
-
-      if (err) {
-        requestListener.res.statusCode = 500;
-        requestListener.res.end('500 Internal Error');
-      } else {
-        requestListener.res.statusCode = 404;
-        requestListener.res.end('404 not found');
-      }
-      return;
-    }
-
-    var pos = requestListener._findNextMiddleware(err);
-    requestListener.currentPos = pos + 1;
-    try {
-      if (requestListener.stack[pos].callAsMiddleware) {
-        requestListener.stack[pos].callAsMiddleware(err, requestListener.req,
-            requestListener.res, requestListener);
-        return;
-      }
-      if (err) {
-        requestListener.stack[pos](err, requestListener.req, requestListener.res,
-            requestListener.next);
-      } else {
-        requestListener.stack[pos](requestListener.req, requestListener.res,
-            requestListener.next);
-      }
-    } catch (e) {
-      requestListener.res.statusCode = 500;
-      requestListener.res.end('500 Internal Error');
-    }
-  }
   return requestListener;
-}
+};
