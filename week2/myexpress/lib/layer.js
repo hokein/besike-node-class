e3459d943de908ffc1d3577908281f0a4fe554a3
Haojian Wu <hokein.wu@gmail.com>
2015-01-14 10:10:36 +0800

    Implement path parameters extraction.

--- week2/myexpress/lib/layer.js
+++ week2/myexpress/lib/layer.js
@@ -1,13 +1,22 @@
+var pathToRegexp = require('path-to-regexp');
+
 module.exports = Layer;
 
 function Layer(path, middleware) {
-  this.path = path;
+  this.keys = [];
+  if (path[path.length-1] == '/')
+    this.re = pathToRegexp(path.slice(0,-1), this.keys, {end: false});
+  else
+    this.re = pathToRegexp(path, this.keys, {end: false});
   this.handle = middleware;
 }
 
 Layer.prototype.match = function(matchPath) {
-  var reg = new RegExp('^' + this.path);
-  if (matchPath.search(reg) != -1)
-    return this;
-  return undefined;
+  var m = this.re.exec(decodeURIComponent(matchPath));
+  if (!m)
+    return undefined;
+  var result = {path: m[0], params:{}};
+  for (var i = 0; i < this.keys.length; ++i)
+    result.params[(this.keys[i])['name']] = m[i+1];
+  return result;
 }
