53177e89ea5e639d65a32dc0fb9de61367e2373f
Haojian Wu <hokein.wu@gmail.com>
2015-01-14 11:11:53 +0800

    Implement prefix path trimming for embedded app.

--- week2/myexpress/index.js
+++ week2/myexpress/index.js
@@ -7,10 +7,28 @@ function isErrorHandler(func) {
 
 module.exports = function() {
   var requestListener = function(req, res, parentNext) {
+    requestListener.handle(req, res, parentNext);
+  }
+
+  requestListener.stack = [];
+  requestListener.listen = function() {
+    var server = http.createServer(requestListener);
+    server.listen.apply(server, arguments);
+    return server;
+  }
+  requestListener.use = function() {
+    if (arguments.length == 1)
+      requestListener.stack.push(new Layer('/', arguments[0]));
+    else
+      requestListener.stack.push(new Layer(arguments[0], arguments[1]));
+    return requestListener;
+  }
+  requestListener.handle = function(req, res, parentNext) {
     var currentPos = 0;
     function next(err) {
       if (currentPos >= requestListener.stack.length) {
         if (parentNext) {
+          req.url = req.url_bak;
           parentNext(err);
         } else {
           res.statusCode = err? 500:404;
@@ -32,7 +50,13 @@ module.exports = function() {
             middleware(err, req, res, next);
           else if (!err && !isErrorHandler(middleware))
             // call next middleware or subapp
-            middleware(req, res, next);
+            if (middleware.handle) {
+              req.url_bak = req.url;
+              req.url = req.url.slice(result.path.length);
+              middleware.handle(req, res, next);
+            } else {
+              middleware(req, res, next);
+            }
           else // skip to next middleware
             next(err);
         } catch(e) {
@@ -42,20 +66,6 @@ module.exports = function() {
       }
     }
     next();
-  }
-
-  requestListener.stack = [];
-  requestListener.listen = function() {
-    var server = http.createServer(requestListener);
-    server.listen.apply(server, arguments);
-    return server;
-  }
-  requestListener.use = function() {
-    if (arguments.length == 1)
-      requestListener.stack.push(new Layer('/', arguments[0]));
-    else
-      requestListener.stack.push(new Layer(arguments[0], arguments[1]));
-    return requestListener;
   }
   return requestListener;
 };
